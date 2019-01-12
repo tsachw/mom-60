ca619497695e127cc67f9bf86c8eaeeb6912c0c7
Tsach <tsach@kayma.com>
2019-01-12 10:45:15 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="root_cont">
+        <h1>אמא בת 60</h1>
       </div>
     );
   }
