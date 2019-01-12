751ef76a6210200aac70e25285d3e135e366dc1b
Tsach <tsach@kayma.com>
2019-01-12 11:49:13 +0200

    fonts and colors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
+
+global.colors = {
+  background: '#e6e9e1',
+  main: '#f70d3f',
+}
+
 class App extends Component {
+  
   render() {
     return (
       <div className="root_cont">
-        <h1>אמא בת 60</h1>
+      <div style={{backgroundColor: global.colors.main, borderRadius: '100%', width: 200, height:200}}/>
+        <h1 style={{backgroundColor: 'white'}}>שישימא</h1>
+        <h3>שישו ושימחו ביום ההולדת 60</h3>
+        <p>אוכל והפתעות שבועיות</p>
       </div>
     );
   }
